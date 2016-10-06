6112d1c80a3e550de1cd287eecd08bf0ba876538
Henrique <hlopes13@hotmail.com>
2016-10-06 11:29:11 -0300

    Arquivo de perguntas com algumas possibilidades

---
+++ perguntas.pl
@@ -0,0 +1,21 @@
+perguntaIdade1:-write('O paciante é criança?').
+perguntaIdade2:-write('O paciante é adulto?').
+
+perguntaOlhos:-write('O paciante está com os olhos vermelhos?').
+
+perguntaFebre1:-write('O paciante está com febre?').
+perguntaFebre2:-write('O paciante está com febre é acima dos 39 graus?').
+
+perguntaFisico1:-write('O paciante está com dor itensa?').
+perguntaFisico2:-write('O paciante está com manchas vermelhas na pele?').
+perguntaFisico3:-write('O paciante está com dor de cabeça?').
+perguntaFisico4:-write('O paciante está com dores abdominais?').
+perguntaFisico5:-write('O paciante está com vômitos constantes?').
+perguntaFisico6:-write('O paciante está com calafrios?').
+
+perguntaHepatica:-write('O paciante está com manifestações de insuficiência hepática?').
+perguntaHemorragia:-write('O paciente está com qualquer tipo de indicação hemorrágica?').
+
+% Dengue hemorragica
+perguntaDengue1-write('O cliente já teve dengue?').
+perguntaDengue2-write('O cliente está queda ou ausência de pressão arterial?').
